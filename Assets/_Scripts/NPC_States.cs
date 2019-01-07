cad96489faccb3d709392656969e7de6458bcb9d
rycon2424 <rycon2424@users.noreply.github.com>
2019-01-07 12:48:09 +0100

    Art/Amanda/TextBox

---
+++ Assets/_Scripts/NPC_States.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC_States : MonoBehaviour {
+
+    public bool idleForward, idleBack, idleLeft, idleRight;
+
+    public bool walkForward, walkBack, walkLeft, walkRight;
+
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update ()
+    {
+        if (idleForward)
+        {
+            anim.Play("Idle_Forward");
+        }
+        else if (idleBack)
+        {
+            anim.Play("Idle_Back");
+        }
+        else if (idleLeft)
+        {
+            anim.Play("Idle_Left");
+        }
+        else if (idleRight)
+        {
+            anim.Play("Idle_Right");
+        }
+        else if (walkForward)
+        {
+            anim.Play("Walk_Forward");
+        }
+        else if (walkBack)
+        {
+            anim.Play("Walk_Back");
+        }
+        else if (walkLeft)
+        {
+            anim.Play("Walk_Left");
+        }
+        else if (walkRight)
+        {
+            anim.Play("Walk_Right");
+        }
+	}
+}
---
+++ Assets/_Scripts/TextAnimation.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextAnimation : MonoBehaviour
+{
+
+    public Text dialogueText;
+    public float delay;
+    private string fullText;
+
+    public string[] sentences = new string[3];
+
+    public int waitTime;
+
+    public GameObject Canvas;
+
+    private string currentText = "";
+    // Use this for initialization
+    void Start()
+    {
+        fullText = sentences[0];
+        StartCoroutine(ShowText());
+        StartCoroutine(Text());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator ShowText()
+    {
+        for (int i = 0; i < fullText.Length; i++)
+        {
+            currentText = fullText.Substring(0, i);
+            dialogueText.GetComponent<Text>().text = currentText;
+            yield return new WaitForSeconds(delay);
+        }
+    }
+
+    IEnumerator Text()
+    {
+        yield return new WaitForSeconds(waitTime);
+        for (int i = 1; i < sentences.Length; i++)
+        {
+            fullText = sentences[i];
+            StartCoroutine(ShowText());
+            yield return new WaitForSeconds(waitTime);
+        }
+        yield return new WaitForSeconds(3);
+        Destroy();
+    }
+
+    public void Destroy()
+    {
+        Destroy(Canvas);
+    }
+}
