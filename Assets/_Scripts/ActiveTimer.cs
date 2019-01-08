8852768c8b797c061d16e3e3ce26fcf95258fb77
rycon2424 <rycon2424@users.noreply.github.com>
2019-01-08 15:11:33 +0100

    Chars and player

---
+++ Assets/_Scripts/ActiveTimer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveTimer : MonoBehaviour
+{
+
+    public float time;
+    public GameObject activeObject;
+
+    void Start()
+    {
+        StartCoroutine(Timer());
+        activeObject.SetActive(false);
+    }
+    
+    IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(time);
+        activeObject.SetActive(true);
+    }
+
+}
