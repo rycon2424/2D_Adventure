0c06ed5f88d6b5c48d36692a5d0f4a116b0a6d62
rycon2424 <rycon2424@users.noreply.github.com>
2019-01-08 16:18:56 +0100

    Animations Battle Menu

---
+++ Assets/_Scripts/BattleMenu.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleMenu : MonoBehaviour
+{
+    public Animator choiceAnimation;
+    public int whatCharacter = 1;
+    [Header("Finger Options")]
+    public bool characterSelected;
+    public GameObject fingerObject;
+    public Animator fingerAnimation;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        if (!characterSelected)
+        {
+            SelectCharacter();
+        }
+    }
+
+    void SelectCharacter()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (whatCharacter == 3)
+            {
+                whatCharacter = 1;
+            }
+            else
+            {
+                whatCharacter++;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (whatCharacter == 1)
+            {
+                whatCharacter = 3;
+            }
+            else
+            {
+                whatCharacter--;
+            }
+        }
+        if (whatCharacter == 1)
+        {
+            fingerAnimation.Play("FingerAnimationPlayer1");
+        }
+        else if (whatCharacter == 2)
+        {
+            fingerAnimation.Play("FingerAnimationPlayer2");
+        }
+        else if (whatCharacter == 3)
+        {
+            fingerAnimation.Play("FingerAnimationPlayer3");
+        }
+    }
+
+}
