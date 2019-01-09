618bbc76ac24e3ffa59b3fe50a18ef18f3e91466
Tijn van den Broek <36229295+TijnWorks@users.noreply.github.com>
2019-01-09 10:16:00 +0100

    Added animation for teleport transition
    Teleporting from house outside to house inside smooths over with a fade animation.

--- Assets/_Scripts/Doors.cs
+++ Assets/_Scripts/Doors.cs
@@ -7,12 +7,29 @@ public class Doors : MonoBehaviour
 
     public Transform teleportTo;
 
+    public GameObject ui;
+    private Animator uiAnimator;
+
+    private GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        uiAnimator = GameObject.FindGameObjectWithTag("PlayerCanvas").GetComponent<Animator>();
+    }
+
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.CompareTag("Player"))
         {
-            col.gameObject.transform.position = teleportTo.transform.position;
+            StartCoroutine(Transition());
         }
+    }
+
+    IEnumerator Transition() {
+        uiAnimator.Play("FadeAnimation");
+        yield return new WaitForSecondsRealtime(0.75f);
+        player.transform.position = teleportTo.transform.position;
     }
 
 }
