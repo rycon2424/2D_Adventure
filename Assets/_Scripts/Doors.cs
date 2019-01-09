e9d96971a54d0e340982b1e762ce45cb5186b122
Tijn van den Broek <36229295+TijnWorks@users.noreply.github.com>
2019-01-09 09:47:30 +0100

    Player + House fixed

---
+++ Assets/_Scripts/Doors.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doors : MonoBehaviour
+{
+
+    public Transform teleportTo;
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            col.gameObject.transform.position = teleportTo.transform.position;
+        }
+    }
+
+}
