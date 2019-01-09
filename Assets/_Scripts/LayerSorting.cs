674049bc3d7b4e0c2c3a26c1b0a1bd8b9591908b
Tijn van den Broek <36229295+TijnWorks@users.noreply.github.com>
2019-01-09 12:21:35 +0100

    Added a layer sorting script

---
+++ Assets/_Scripts/LayerSorting.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerSorting : MonoBehaviour
+{
+
+    private SpriteRenderer sr;
+    private float yPos;
+
+    private void Start()
+    {
+        sr = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        sr.sortingOrder = Mathf.RoundToInt(yPos = gameObject.transform.position.y + 100);
+    }
+
+}
