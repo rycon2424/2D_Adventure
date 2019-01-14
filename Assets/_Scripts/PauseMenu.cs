ae521dd9665b4fe8bf6a3cf858c1789502075a98
Tijn van den Broek <36229295+TijnWorks@users.noreply.github.com>
2019-01-14 14:12:40 +0100

    PauseMenu started

--- Assets/_Scripts/PauseMenu.cs
+++ Assets/_Scripts/PauseMenu.cs
@@ -7,89 +7,187 @@ public class PauseMenu : MonoBehaviour
 {
 
     public Text party, inventory, skills, save, exit;
+    public GameObject partyMenu, inventoryMenu, skillsMenu, saveMenu;
+    private enum Menus {none,party,inventory,skills,save};
+    private Menus currentMenu;
     private int active;
+    private bool menuActive = true;
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        if (menuActive)
         {
-            if (active <= 4)
+            if (Input.GetKeyDown(KeyCode.DownArrow))
             {
-                active++;
+                if (active <= 4)
+                {
+                    active++;
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                if (active >= -1)
+                {
+                    active--;
+                }
             }
         }
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        MenuSelector();
+
+        switch (currentMenu)
         {
-            if (active >= -1)
-            {
-                active--;
-            }
+            case Menus.party:
+                Party();
+            break;
+            case Menus.inventory:
+                Inventory();
+            break;
+            case Menus.skills:
+                Skills();
+            break;
+            case Menus.save:
+                Save();
+            break;
+            default:
+                currentMenu = Menus.none;
+            break;
         }
-        TextHighlight();
 
     }
 
-    private void TextHighlight() {
-
+    private void MenuSelector()
+    {
         switch (active)
+            {
+                case -1:
+                    party.color = Color.white;
+                    party.fontStyle = FontStyle.Normal;
+                    active = 4;
+                break;
+                case 0:
+                    party.color = Color.yellow;
+                    inventory.color = Color.white;
+
+                    party.fontStyle = FontStyle.Bold;
+                    inventory.fontStyle = FontStyle.Normal;
+                    if (Input.GetKeyDown(KeyCode.Space))
+                    {
+                        partyMenu.SetActive(true);
+                        menuActive = false;
+                        currentMenu = Menus.party;
+                    }
+                break;
+                case 1:
+                    party.color = Color.white;
+                    inventory.color = Color.yellow;
+                    skills.color = Color.white;
+
+                    party.fontStyle = FontStyle.Normal;
+                    inventory.fontStyle = FontStyle.Bold;
+                    skills.fontStyle = FontStyle.Normal;
+                    if (Input.GetKeyDown(KeyCode.Space))
+                    {
+                        inventoryMenu.SetActive(true);
+                        menuActive = false;
+                        currentMenu = Menus.inventory;
+                    }
+                break;
+                case 2:
+                    inventory.color = Color.white;
+                    skills.color = Color.yellow;
+                    save.color = Color.white;
+
+                    inventory.fontStyle = FontStyle.Normal;
+                    skills.fontStyle = FontStyle.Bold;
+                    save.fontStyle = FontStyle.Normal;
+                    if (Input.GetKeyDown(KeyCode.Space))
+                    {
+                        skillsMenu.SetActive(true);
+                        menuActive = false;
+                    currentMenu = Menus.skills;
+                    }
+                break;
+                case 3:
+                    skills.color = Color.white;
+                    save.color = Color.yellow;
+                    exit.color = Color.white;
+
+                    skills.fontStyle = FontStyle.Normal;
+                    save.fontStyle = FontStyle.Bold;
+                    exit.fontStyle = FontStyle.Normal;
+                    if (Input.GetKeyDown(KeyCode.Space))
+                    {
+                        saveMenu.SetActive(true);
+                        menuActive = false;
+                        currentMenu = Menus.save;
+                    }
+                break;
+                case 4:
+                    save.color = Color.white;
+                    exit.color = Color.yellow;
+
+                    save.fontStyle = FontStyle.Normal;
+                    exit.fontStyle = FontStyle.Bold;
+                    if (Input.GetKeyDown(KeyCode.Space))
+                    {
+                        Exit();
+                    }
+                break;
+                case 5:
+                    exit.color = Color.white;
+
+                    exit.fontStyle = FontStyle.Normal;
+                    active = 0;
+                break;
+                default:
+                    active = 0;
+                break;
+            }
+    }
+
+    private void Party()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
         {
-            case -1:
-                party.color = Color.white;
-                party.fontStyle = FontStyle.Normal;
-                active = 4;
-                break;
-            case 0:
-                party.color = Color.yellow;
-                inventory.color = Color.white;
-
-                party.fontStyle = FontStyle.Bold;
-                inventory.fontStyle = FontStyle.Normal;
-                break;
-            case 1:
-                party.color = Color.white;
-                inventory.color = Color.yellow;
-                skills.color = Color.white;
-
-                party.fontStyle = FontStyle.Normal;
-                inventory.fontStyle = FontStyle.Bold;
-                skills.fontStyle = FontStyle.Normal;
-                break;
-            case 2:
-                inventory.color = Color.white;
-                skills.color = Color.yellow;
-                save.color = Color.white;
-
-                inventory.fontStyle = FontStyle.Normal;
-                skills.fontStyle = FontStyle.Bold;
-                save.fontStyle = FontStyle.Normal;
-                break;
-            case 3:
-                skills.color = Color.white;
-                save.color = Color.yellow;
-                exit.color = Color.white;
-
-                skills.fontStyle = FontStyle.Normal;
-                save.fontStyle = FontStyle.Bold;
-                exit.fontStyle = FontStyle.Normal;
-                break;
-            case 4:
-                save.color = Color.white;
-                exit.color = Color.yellow;
-
-                save.fontStyle = FontStyle.Normal;
-                exit.fontStyle = FontStyle.Bold;
-                break;
-            case 5:
-                exit.color = Color.white;
-
-                exit.fontStyle = FontStyle.Normal;
-                active = 0;
-                break;
-            default:
-                active = 0;
-                break;
+            partyMenu.SetActive(false);
+            currentMenu = Menus.none;
+            menuActive = true;
         }
-
+    }
+    private void Inventory()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            inventoryMenu.SetActive(false);
+            currentMenu = Menus.none;
+            menuActive = true;
+        }
+    }
+    private void Skills()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            skillsMenu.SetActive(false);
+            currentMenu = Menus.none;
+            menuActive = true;
+        }
+    }
+    private void Save()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            saveMenu.SetActive(false);
+            currentMenu = Menus.none;
+            menuActive = true;
+        }
+    }
+    private void Exit()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
     }
 
 }
